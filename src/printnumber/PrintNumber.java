334dc4ae35a4cba413cd0ee9c49098dca7e0aaf5
Binh Nguyen <binhnguyen.plk91@gmail.com>
2017-03-24 21:09:58 +0700

    Update

--- src/printnumber/PrintNumber.java
+++ src/printnumber/PrintNumber.java
@@ -10,7 +10,7 @@ public class PrintNumber {
     public static void main(String[] args) {
         int i;
         for(i=0;i<10;i++){
-            System.out.println("i="+i);            
+            System.out.println("The value of i = "+i);            
         }
         System.out.println("All Done!");
     }
