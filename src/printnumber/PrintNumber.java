b92467e6fe3fc7918d6fa98e706b0de8d89db1d2
Binh Nguyen <binhnguyen.plk91@gmail.com>
2017-03-24 21:05:52 +0700

    Remove comment

--- src/printnumber/PrintNumber.java
+++ src/printnumber/PrintNumber.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package printnumber;
 
 /**
@@ -11,9 +7,6 @@ package printnumber;
  */
 public class PrintNumber {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int i;
         for(i=0;i<10;i++){
